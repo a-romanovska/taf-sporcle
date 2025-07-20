480dcace197ad2806308c910d56a6956ebe12284
a.romanovska <a.romanovska.dev@gmail.com>
2025-07-20 18:38:48 +0300

    add endpoints

--- src/main/java/com/sporcle/finals/Endpoints.java
+++ src/main/java/com/sporcle/finals/Endpoints.java
@@ -11,7 +11,9 @@ public class Endpoints {
     public static final String VIDEOS = BASE_URL + "/videos/";
     public static final String PRIVATE_EVENTS = EVENTS + "private-events/";
     public static final String REMOVE_ADS = BASE_URL + "/memberships/?rp=user_box_remove_ads_link";
-
+    public static final String COMMUNITY_GUIDELINES = BASE_URL + "/community/";
+    public static final String FORGOT_PASSWORD = BASE_URL + "/forgot/";
+    public static final String FACEBOOK_ACCOUNT = BASE_URL + "/facebook-account/";
     public static final String PART_OF_CONTINUE_WITH_GOOGLE = "accounts.google.com";
     public static final String PART_OF_CONTINUE_WITH_APPLE = "appleid.apple.com";
 }
