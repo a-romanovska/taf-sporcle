38b859b3328013e218ba66e4c6db94d631634db0
a.romanovska <a.romanovska.dev@gmail.com>
2025-07-20 18:40:08 +0300

    update pages

--- src/main/java/com/sporcle/ui/pages/BasePage.java
+++ src/main/java/com/sporcle/ui/pages/BasePage.java
@@ -2,13 +2,18 @@ package com.sporcle.ui.pages;
 
 import com.sporcle.ui.driver.DriverManager;
 import com.sporcle.ui.forms.BaseForm;
+import com.sporcle.ui.forms.LogInForm;
 import com.sporcle.ui.forms.ProductBarForm;
+import com.sporcle.ui.forms.RegistrationForm;
 import org.openqa.selenium.By;
 
 public abstract class BasePage {
     protected String URL;
     protected final String title;
     protected static final By productBar = By.id("product-bar");
+    protected final By logInForm = By.id("login-modal");
+    protected final By registrationForm = By.id("registration-modal");
+    protected final By settingsForm = By.id("user-dropdown");
 
     protected BasePage(String endpoint, String title) {
         this.URL = endpoint;
@@ -51,15 +56,50 @@ public abstract class BasePage {
         return (ProductBarForm) getFormWhenVisible(productBar, ProductBarForm.class);
     }
 
-    public static void switchToNextWindow(){
+    public static void switchToNextWindow() {
         DriverManager.switchToNextWindow();
     }
 
-    public static void closeCurrentWindow(){
+    public static void closeCurrentWindow() {
         DriverManager.closeCurrentWindow();
     }
 
-    public static void switchToOriginalWindow(){
+    public static void switchToOriginalWindow() {
         DriverManager.switchToOriginalWindow();
+    }
+
+    //get visible form
+    public LogInForm getLogInFormWhenVisible() {
+        return (LogInForm) getFormWhenVisible(logInForm, LogInForm.class);
+    }
+
+    public RegistrationForm getRegistrationFormWhenVisible() {
+        return (RegistrationForm) getFormWhenVisible(registrationForm, RegistrationForm.class);
+    }
+
+    //check if visible
+    public boolean logInFormIsVisible() {
+        return checkVisibilityState(logInForm, true);
+    }
+
+    public boolean registrationFormIsVisible() {
+        return checkVisibilityState(registrationForm, true);
+    }
+
+    public boolean settingsFormIsVisible() {
+        return checkVisibilityState(settingsForm, true);
+    }
+
+    //check if invisible
+    public boolean logInFormIsInvisible() {
+        return checkVisibilityState(logInForm, false);
+    }
+
+    public boolean registrationFormIsInvisible() {
+        return checkVisibilityState(registrationForm, false);
+    }
+
+    public boolean settingsFormIsInvisible() {
+        return checkVisibilityState(settingsForm, false);
     }
 }
--- src/main/java/com/sporcle/ui/pages/HomePage.java
+++ src/main/java/com/sporcle/ui/pages/HomePage.java
@@ -2,36 +2,9 @@ package com.sporcle.ui.pages;
 
 import com.sporcle.finals.Endpoints;
 import com.sporcle.finals.Titles;
-import com.sporcle.ui.forms.LogInForm;
-import org.openqa.selenium.By;
 
 public class HomePage extends BasePage {
-    private final By logInForm = By.id("login-modal");
-    private final By settingsForm = By.id("user-dropdown");
-
     public HomePage() {
         super(Endpoints.HOME, Titles.HOME_PAGE);
-    }
-
-    //get visible form
-    public LogInForm getLogInFormWhenVisible() {
-        return (LogInForm) getFormWhenVisible(logInForm, LogInForm.class);
-    }
-
-    //check visibility
-    public boolean logInFormIsVisible() {
-        return checkVisibilityState(logInForm, true);
-    }
-
-    public boolean logInFormIsInvisible() {
-        return checkVisibilityState(logInForm, false);
-    }
-
-    public boolean settingsFormIsVisible() {
-        return checkVisibilityState(settingsForm, true);
-    }
-
-    public boolean settingsFormIsInvisible() {
-        return checkVisibilityState(settingsForm, false);
     }
 }
