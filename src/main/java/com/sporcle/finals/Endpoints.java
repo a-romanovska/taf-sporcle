b03be4132f47c9f403f19f300d65cca2da274ee0
a.romanovska <a.romanovska.dev@gmail.com>
2025-07-20 14:51:10 +0300

    Refactor

---
+++ src/main/java/com/sporcle/finals/Endpoints.java
@@ -0,0 +1,16 @@
+package com.sporcle.finals;
+
+import com.sporcle.utils.PropertiesUtils;
+
+public class Endpoints {
+    public static final String BASE_URL = PropertiesUtils.getBaseUrl();
+    public static final String HOME = BASE_URL + "/";
+    public static final String EVENTS = BASE_URL + "/events/";
+    public static final String QUIZ_CREATION = BASE_URL + "/create/";
+    public static final String COMMUNITY = BASE_URL + "/groups/";
+    public static final String VIDEOS = BASE_URL + "/videos/";
+    public static final String PRIVATE_EVENTS = EVENTS + "private-events/";
+    public static final String REMOVE_ADS = BASE_URL + "/memberships/?rp=user_box_remove_ads_link";
+
+    public static final String PART_OF_CONTINUE_WITH_GOOGLE = "accounts.google.com";
+}
