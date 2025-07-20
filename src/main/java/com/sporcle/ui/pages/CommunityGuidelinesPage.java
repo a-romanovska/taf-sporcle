9fd09a9d0b54ad3d4e934662dda85433245ec7d8
a.romanovska <a.romanovska.dev@gmail.com>
2025-07-20 18:39:54 +0300

    add pages

---
+++ src/main/java/com/sporcle/ui/pages/CommunityGuidelinesPage.java
@@ -0,0 +1,10 @@
+package com.sporcle.ui.pages;
+
+import com.sporcle.finals.Endpoints;
+import com.sporcle.finals.Titles;
+
+public class CommunityGuidelinesPage extends BasePage {
+    public CommunityGuidelinesPage() {
+        super(Endpoints.COMMUNITY_GUIDELINES, Titles.COMMUNITY_GUIDELINES);
+    }
+}
---
+++ src/main/java/com/sporcle/ui/pages/FacebookAccountPage.java
@@ -0,0 +1,10 @@
+package com.sporcle.ui.pages;
+
+import com.sporcle.finals.Endpoints;
+import com.sporcle.finals.Titles;
+
+public class FacebookAccountPage extends BasePage{
+    public FacebookAccountPage() {
+        super(Endpoints.FACEBOOK_ACCOUNT, Titles.FACEBOOK_ACCOUNT);
+    }
+}
