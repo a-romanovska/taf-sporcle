08b472d31e04759c62f2fedcbf02d70ba380edd4
a.romanovska <a.romanovska.dev@gmail.com>
2025-07-21 17:50:50 +0300

    update DEFAULT_EXPLICIT_WAIT_TIMEOUT_SECONDS to 5

--- src/main/java/com/sporcle/finals/Finals.java
+++ src/main/java/com/sporcle/finals/Finals.java
@@ -1,6 +1,6 @@
 package com.sporcle.finals;
 
 public class Finals {
-    public static final int DEFAULT_EXPLICIT_WAIT_TIMEOUT_SECONDS = 10;
+    public static final int DEFAULT_EXPLICIT_WAIT_TIMEOUT_SECONDS = 5;
     public static final String EMPTY_STRING = "";
 }
