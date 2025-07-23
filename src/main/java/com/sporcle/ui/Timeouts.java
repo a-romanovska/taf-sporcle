e6bc6d08d07535f9f68a3d8e8d3623da4cf8e4ac
a.romanovska <a.romanovska.dev@gmail.com>
2025-07-23 18:00:12 +0300

    update default timeout

--- src/main/java/com/sporcle/ui/Timeouts.java
+++ src/main/java/com/sporcle/ui/Timeouts.java
@@ -1,5 +1,5 @@
 package com.sporcle.ui;
 
 public class Timeouts {
-    public static final int DEFAULT_EXPLICIT_WAIT_TIMEOUT_SECONDS = 5;
+    public static final int DEFAULT_EXPLICIT_WAIT_TIMEOUT_SECONDS = 15;
 }
